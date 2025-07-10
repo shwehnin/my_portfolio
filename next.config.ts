d89e00aa2031de342343401c9cf66cb46502bc59
shwehnin <hninhnin6398@gmail.com>
2025-07-11 01:35:46 +0700

    fixed coludflare config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,9 +6,5 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
 };
-
-if(process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
